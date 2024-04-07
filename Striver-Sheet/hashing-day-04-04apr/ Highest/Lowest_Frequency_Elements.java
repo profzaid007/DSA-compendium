0d0c7dc475e6e5f0c800d76c8da31c4559cd3522
Mohammed Zaid M <codezaidprof@gmail.com>
2024-04-07 10:00:09 +0530

    Hash done final

--- Striver-Sheet/hashing-day-04-04apr/ Highest/Lowest_Frequency_Elements.java
+++ Striver-Sheet/hashing-day-04-04apr/ Highest/Lowest_Frequency_Elements.java
@@ -21,8 +21,9 @@ import java.util.Map.Entry;
 
 class Main{
 	public static int[] getFrequencies(int []v) {
+       Arrays.sort(v);
         HashMap<Integer,Integer> map = new HashMap<>();
-
+		int [] arr = new int[2];
         for(int i=0;i<v.length;i++){
             int key = v[i];
             int freq =0;
@@ -32,24 +33,47 @@ class Main{
             freq++;
             map.put(key,freq);
         }
-        int[] arr = new int[2];
-        
-        int max = (Collections.max(map.values()));
-        int min = (Collections.min(map.values()));
-		for(Entry<Integer,Integer> entry : map.entrySet()){
-			if(entry.getValue()==max){
-				arr[0] =entry.getKey();
-			}
-			else if(entry.getValue()==min){
-				arr[1] = entry.getKey();
-				break;
-			}
-		}
+        int maxf =0;
+        int minf = v.length;
+        int minv=0;
+        int maxv = 0;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+        	int ctr = entry.getValue();
+        	int key = entry.getKey();
+        	if(ctr<=minf){
+                if(ctr==minf){
+                    minv = Math.min(minv,key);
+                }else{
+                    minv = key;
+        		    minf = ctr;
+                }
+        		
+        	}
+        	if(ctr>=maxf){
+                if(ctr==maxf){
+                    maxv = Math.min(maxv,key);
+                }else{
+                    maxv = key;
+        		    maxf = ctr;
+                }
+            }
+        }
+        arr[0]= maxv;
+        if(minv==0){
+        	arr[1]=maxv;
+        }else{
+        	arr[1] = minv;
+        }
         return arr;
     }
 	public static void main(String[]args){
 		Scanner sc = new Scanner(System.in);
-		int[] v = {1, 2, 3, 1, 1, 4};
+		int t = sc.nextInt();
+		int[] v = new int[t];
+		for(int i=0;i<t;i++){
+			v[i]=sc.nextInt();
+		}
+		Arrays.sort(v);
 		int[] arr = getFrequencies(v);
 		for(int i=0;i<arr.length;i++){
 			System.out.print(arr[i]+" ");
